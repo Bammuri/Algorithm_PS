765098b0a5749e198035afb23bfa46a301e52048
HyunJun <shj1504@korea.ac.kr>
2019-11-03 14:06:45 +0900

    19.11.03

---
+++ 191101_PRO_가사검색(효율성Error).cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> words, vector<string> queries) {
+	int wsize = words.size();
+	int qsize = queries.size();
+
+	vector<int> answer(qsize, 0);
+
+	for (int j = 0; j<qsize; j++){
+		int qjsize = queries[j].size();
+		bool front;
+		int k = 0;
+		if (queries[j][qjsize - 1] == '?'){
+			front = false;
+		}
+		else {
+			front = true;
+			while (queries[j][k] == '?'){
+				k++;
+			}
+		}
+		int ktmp = k;
+		for (int i = 0; i<wsize; i++){
+			if (words[i].size() != qjsize) continue;
+
+			for (k = ktmp; k<words[i].size(); k++){
+				if (words[i][k] != queries[j][k] && queries[j][k] != '?')break;
+
+				if (front){
+					if (k == words[i].size() - 1)answer[j]++;
+				}
+				else {
+					if (queries[j][k] == '?'){ answer[j]++; break; }
+				}
+			}
+		}
+	}
+
+	return answer;
+}
