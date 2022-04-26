76f70376084b336264859141f3035d724d3bc6b6
HyunJun <shjj1504@gmail.com>
2022-04-26 09:31:34 +0900

    220426

---
+++ 220426_LeetCode_154_Min_Cost_to_Connect_All_Points.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <queue>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+	int distance(const vector<int>& point1, const vector<int>& point2) {
+		return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1]);
+	}
+	int minCostConnectPoints(const vector<vector<int>>& points) {
+		int total = 0;
+		priority_queue<pair<int, int>> pq;
+		pq.push({ 0, 0 });
+		unordered_set<int> visited;
+		while (!pq.empty()) {
+			auto curr = pq.top();
+			pq.pop();
+			if (visited.find(curr.second) != visited.end())
+				continue;
+			total -= curr.first;
+			visited.insert(curr.second);
+			for (int i = 0; i < points.size(); i++) {
+				if (i != curr.second && visited.find(i) == visited.end()) {
+					pq.push({ -distance(points[curr.second], points[i]), i });
+				}
+			}
+		}
+		return total;
+	}
+};
